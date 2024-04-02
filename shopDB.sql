d564f70af72301e7a183a37bbbdf90391181ff43
Hwangsj-qk <sujeong7191@naver.com>
2024-04-02 10:03:17 +0900

    SQL 공부 첫번째 커밋

---
+++ shopDB.sql
@@ -0,0 +1,131 @@
+CREATE DATABASE  IF NOT EXISTS `shopdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `shopdb`;
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: shopdb
+-- ------------------------------------------------------
+-- Server version	8.0.36
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `index_tbl`
+--
+
+DROP TABLE IF EXISTS `index_tbl`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `index_tbl` (
+  `first_name` varchar(14) DEFAULT NULL,
+  `last_name` varchar(16) DEFAULT NULL,
+  `hire_date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `index_tbl`
+--
+
+LOCK TABLES `index_tbl` WRITE;
+/*!40000 ALTER TABLE `index_tbl` DISABLE KEYS */;
+/*!40000 ALTER TABLE `index_tbl` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `membertbl`
+--
+
+DROP TABLE IF EXISTS `membertbl`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `membertbl` (
+  `productName` char(4) NOT NULL,
+  `cost` int NOT NULL,
+  `makeDate` date DEFAULT NULL,
+  `company` char(5) DEFAULT NULL,
+  `amount` int NOT NULL,
+  PRIMARY KEY (`productName`,`cost`,`amount`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `membertbl`
+--
+
+LOCK TABLES `membertbl` WRITE;
+/*!40000 ALTER TABLE `membertbl` DISABLE KEYS */;
+/*!40000 ALTER TABLE `membertbl` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `my test_tbl`
+--
+
+DROP TABLE IF EXISTS `my test_tbl`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `my test_tbl` (
+  `id` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `my test_tbl`
+--
+
+LOCK TABLES `my test_tbl` WRITE;
+/*!40000 ALTER TABLE `my test_tbl` DISABLE KEYS */;
+/*!40000 ALTER TABLE `my test_tbl` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `producttbl`
+--
+
+DROP TABLE IF EXISTS `producttbl`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `producttbl` (
+  `memberID` char(8) NOT NULL,
+  `memberName` char(5) NOT NULL,
+  `memberAddress` char(20) DEFAULT NULL,
+  PRIMARY KEY (`memberID`,`memberName`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `producttbl`
+--
+
+LOCK TABLES `producttbl` WRITE;
+/*!40000 ALTER TABLE `producttbl` DISABLE KEYS */;
+/*!40000 ALTER TABLE `producttbl` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'shopdb'
+--
+
+--
+-- Dumping routines for database 'shopdb'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-04-01 16:10:55
