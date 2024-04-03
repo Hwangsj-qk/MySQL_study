b616e79956d40b0422eed232670e6bfc8d34c152
Hwangsj-qk <sujeong7191@naver.com>
2024-04-03 11:20:55 +0900

    240403

--- 240402/테이블 생성 및 제약조건.sql
+++ 240402/테이블 생성 및 제약조건.sql
@@ -60,3 +60,147 @@ CREATE TABLE adults (
 DESCRIBE adults;
 
 SELECT * FROM adults;
+
+-- DEFAULT 제약조건
+-- 각 컬럼에 대한 기본 값 지정 
+CREATE TABLE persons(
+	id INT PRIMARY KEY,
+	name VARCHAR(100) NOT NULL,
+	status VARCHAR(50) DEFAULT '활동중',			-- 상태 열이 기본값으로 '활동중' (직접 입력하면 그 값이 들어가고 비워두면 기본으로 '활동중')
+    join_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP	-- 가입일 기본값으로 현재 시간
+);
+
+DESCRIBE persons;
+SELECT * FROM persons;
+-- DROP TABLE persons;
+
+-- AUTO_INCREMENT : 데이터베이스에서 자동으로 값이 증가하는 열 설정
+-- 일반적으로 기본키 컬럼에 사용됨. 
+CREATE TABLE products (
+	product_id INT AUTO_INCREMENT PRIMARY KEY,		-- 아이디를 입력하지 않아도 1부터 오름차순으로 자동으로 부여됨 
+    product_name VARCHAR(255) NOT NULL
+);
+
+DESCRIBE products;
+SELECT * FROM products;
+
+use test_db;
+DROP TABLE employees;
+-- 외래 키 - 참조무결성 제약조건
+-- 한 테이블의 컬럼이 다른 테이블의 키(기본 키)를 참조 
+
+-- 부서 테이블
+CREATE TABLE departments (
+	department_id INT PRIMARY KEY,
+    department_name VARCHAR(255) NOT NULL
+);
+
+-- 직원 테이블 생성
+CREATE TABLE employees (
+	employee_id INT PRIMARY KEY,
+    employee_name VARCHAR(255) NOT NULL,
+    department_id INT,
+    -- 직원 테이블 부서 ID는 부서 테이블의 부서 ID를 참조(외래 키 설정)
+    FOREIGN KEY (department_id) REFERENCES departments(department_id)
+);
+
+-- 외래키 컬럼에 참조 위치에 존재하지 않는 값을 넣을 경우 
+-- 참조 무결성을 위반하게 되어 실행되지 않는다. (참조 무결성 제약조건)
+-- 데이터 관계의 일관성을 보장 (부서에 있어야 직원 테이블에서도 오류없이 실행 가능)
+SELECT * FROM employees ;
+SELECT * FROM departments ; 
+
+-- 외래 키 레퍼런스 옵션
+-- 1. CASECADE
+DROP TABLE employees;
+DROP TABLE departments;
+
+CREATE TABLE departments (
+	department_id INT PRIMARY KEY,
+    department_name VARCHAR(255) NOT NULL
+);
+
+-- 직원 테이블 생성
+CREATE TABLE employees (
+	employee_id INT PRIMARY KEY,
+    employee_name VARCHAR(255) NOT NULL,
+    department_id INT,
+    -- 직원 테이블의 부서 ID는 부서 테이블의 부서 ID를 참조 (외래 키 설정)
+    FOREIGN KEY (department_id) REFERENCES departments(department_id)
+    -- 특정 부서가 삭제될 때 해당 부서직원 정보도 모두 삭제
+    ON DELETE CASCADE
+);
+
+DESCRIBE employees;
+-- CASCADE 적용 확인
+SELECT * FROM employees;
+SELECT * FROM departments;
+
+-- 2. SET NULL
+DROP TABLE employees;
+DROP TABLE departments;
+
+CREATE TABLE departments (
+	department_id INT PRIMARY KEY,
+    department_name VARCHAR(255) NOT NULL
+);
+
+-- 직원 테이블 생성
+CREATE TABLE employees (
+	employee_id INT PRIMARY KEY,
+    employee_name VARCHAR(255) NOT NULL,
+    department_id INT,
+    -- 직원 테이블의 부서 ID는 부서 테이블의 부서 ID를 참조 (외래 키 설정)
+    FOREIGN KEY (department_id) REFERENCES departments(department_id)
+    --  특정 부서가 삭제될 때 해당 부서 직원의 부서 ID가 NULL로 설정됨.
+     ON DELETE SET NULL
+);
+
+DESCRIBE employees;
+-- SET NULL 적용 확인
+SELECT * FROM employees;
+SELECT * FROM departments;
+
+-- 3. NO ACTION
+-- 고객 테이블 생성
+CREATE TABLE customers(
+	customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(255) NOT NULL
+);
+
+-- 고객 테이블을 참조하는 주무 테이블 생성
+CREATE TABLE orders(
+	order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+    -- 특정 고객 정보를 삭제하려고 하거나, 고객 ID를 변경하려고 할 때
+    -- 작업을 거부하게 됨 
+    on DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
+
+DESC customers ;
+DESC orders ; 
+
+SELECT * FROM customers ;
+SELECT * FROM orders ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
